bb58f03d2bd0831ec3028b498e680261403a6af4
Preetam Verma <preetamv@amazon.com>
2019-11-01 23:35:59 +0530

    basic setup

---
+++ init/install_package.sh
@@ -0,0 +1,7 @@
+#!/bin/#!/usr/bin/env bash
+
+#HDMI control to change TV source
+ sudo apt-get install cec-utils
+
+#For photo , Command is "raspivid -o video.h264 -t 10000"
+sudo apt-get install raspistill
---
+++ init/update.sh
@@ -0,0 +1,3 @@
+
+sudo apt-get update
+sudo apt-get install oracle-java8-jdk
